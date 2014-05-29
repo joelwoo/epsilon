452480a4b26084df75dae6d488d9ea2a91b1881c
joelwoo <NightWingz@joel-woos-macbook.local>
2014-05-29 15:48:09 +0800

    repo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epsilon::Application.config.secret_token = '923cddf681f4bb4081dec0f2db70bcfca2d3374351a1b2204b62eb2c65ba5f6aacee00e9b27bcae4601336236275ebb12699b37c24e5b1ce3833779738887c4f'
